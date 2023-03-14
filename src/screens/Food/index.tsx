a014d425bdb570200832bc1d8996d4bd06ea145f
Pedro Furlan <pedrofurlan6@gmail.com>
2023-03-14 09:32:20 -0300

    Update index.tsx

--- src/screens/Food/index.tsx
+++ src/screens/Food/index.tsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { Button } from "../../components/Button";
 import { ButtonIcon } from "../../components/ButtonIcon";
 import { Header } from "../../components/Header";
 import { TitleAndSubtitle } from "../../components/TitleAndSubtitle";
-import { ButtonContainer, Container, FoodAmountContainer, SecondTitleContainer, TextFoodAmount, TextFoodKgsOrUnid } from "./styles";
+import {
+  ButtonContainer,
+  Container,
+  FoodAmountContainer,
+  SecondTitleContainer,
+  TextFoodAmount,
+  TextFoodKgsOrUnid,
+} from "./styles";
 
 export function Food() {
-  const [foodAmount, setFoodAmount] = useState(400);
+  const [foodAmount, setFoodAmount] = useState(0);
+
+  function handleAddOneOnFoodAmount() {
+    setFoodAmount((state) => state + 1);
+  }
+
+  function handleRemoveOneOnFoodAmount() {
+    if (foodAmount === 0) {
+      return Alert.alert(
+        "Adicionar e Remover",
+        "Não é possível diminuir mais a quantidade do alimento"
+      );
+    }
+
+    setFoodAmount((state) => state - 1);
+  }
 
   return (
     <Container>
@@ -15,19 +38,25 @@ export function Food() {
       <TitleAndSubtitle title="lorem ipsum" subtitle="lorem ipsum" />
       <SecondTitleContainer>Quantidade</SecondTitleContainer>
 
-    <FoodAmountContainer>
-      <ButtonIcon icon="add" 
-        style={{ marginRight: 12 }}
-      />
-      <TextFoodAmount>{foodAmount}</TextFoodAmount>
-      <TextFoodKgsOrUnid>Unid.</TextFoodKgsOrUnid>
-      <ButtonIcon icon="remove" type="SECONDARY" />
-    </FoodAmountContainer>  
+      <FoodAmountContainer>
+        <ButtonIcon
+          icon="add"
+          style={{ marginRight: 12 }}
+          onPress={handleAddOneOnFoodAmount}
+        />
+        <TextFoodAmount>{foodAmount}</TextFoodAmount>
+        <TextFoodKgsOrUnid>Unid.</TextFoodKgsOrUnid>
+        <ButtonIcon
+          icon="remove"
+          type="SECONDARY"
+          onPress={handleRemoveOneOnFoodAmount}
+        />
+      </FoodAmountContainer>
 
-    <ButtonContainer>
-      <Button title="Salvar" style={{ marginRight: 16 }}/>
-      <Button title="Excluir alimento" type="SECONDARY"/>
-    </ButtonContainer>
+      <ButtonContainer>
+        <Button title="Salvar" style={{ marginRight: 16 }} />
+        <Button title="Excluir alimento" type="SECONDARY" />
+      </ButtonContainer>
     </Container>
-  )
+  );
 }
